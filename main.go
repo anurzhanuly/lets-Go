2ba90dc7b36427961023a32513e4d88e9832d1fb
anurzhanuly <isaxved@gmail.com>
2021-09-23 22:43:32 +0600

    Удаляет ненужные комменты

--- main.go
+++ main.go
@@ -12,8 +12,24 @@ func home(w http.ResponseWriter, r *http.Request) {
 	}
 }
 
+func showSnippet(w http.ResponseWriter, r *http.Request) {
+	_, err := w.Write([]byte("showSnippet"))
+	if err != nil {
+		log.Fatal(err.Error())
+	}
+}
+
+func createSnippet(w http.ResponseWriter, r *http.Request) {
+	_, err := w.Write([]byte("createSnippet"))
+	if err != nil {
+		log.Fatal(err.Error())
+	}
+}
+
 func main() {mux := http.NewServeMux()
 	mux.HandleFunc("/", home)
+	mux.HandleFunc("/snippet", showSnippet)
+	mux.HandleFunc("/snippet/create", createSnippet)
 
 	log.Println("Starting server on :4000")
 	err := http.ListenAndServe(":4000", mux)
