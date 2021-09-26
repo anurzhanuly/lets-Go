b74ce018826891ceb120788a396cf228b65d69c2
anurzhanuly <isaxved@gmail.com>
2021-09-26 15:41:33 +0600

    adds dependency injection of logger

--- cmd/web/handlers.go
+++ cmd/web/handlers.go
@@ -3,12 +3,11 @@ package main
 import (
 	"fmt"
 	template2 "html/template"
-	"log"
 	"net/http"
 	"strconv"
 )
 
-func home(w http.ResponseWriter, r *http.Request) {
+func (app *application) home(w http.ResponseWriter, r *http.Request) {
 	if r.URL.Path != "/" {
 		http.NotFound(w, r)
 
@@ -22,7 +21,7 @@ func home(w http.ResponseWriter, r *http.Request) {
 	}
 	template, err := template2.ParseFiles(templates...)
 	if err != nil {
-		log.Println(err.Error())
+		app.errorLog.Println(err.Error())
 		http.Error(w, "Internal Server Error", 500)
 
 		return
@@ -30,12 +29,12 @@ func home(w http.ResponseWriter, r *http.Request) {
 
 	err = template.Execute(w, nil)
 	if err != nil {
-		log.Println(err.Error())
+		app.errorLog.Println(err.Error())
 		http.Error(w, "Internal Server Error", 500)
 	}
 }
 
-func showSnippet(w http.ResponseWriter, r *http.Request) {
+func (app *application) showSnippet(w http.ResponseWriter, r *http.Request) {
 	id, err := strconv.Atoi(r.URL.Query().Get("id"))
 	if err != nil || id < 1 {
 		http.NotFound(w, r)
@@ -49,7 +48,7 @@ func showSnippet(w http.ResponseWriter, r *http.Request) {
 	}
 }
 
-func createSnippet(w http.ResponseWriter, r *http.Request) {
+func (app *application) createSnippet(w http.ResponseWriter, r *http.Request) {
 	if r.Method != http.MethodPost {
 		w.Header().Set("Allow", http.MethodPost)
 
@@ -60,6 +59,6 @@ func createSnippet(w http.ResponseWriter, r *http.Request) {
 
 	_, err := w.Write([]byte("createSnippet"))
 	if err != nil {
-		log.Fatal(err.Error())
+		app.errorLog.Println(err.Error())
 	}
 }
