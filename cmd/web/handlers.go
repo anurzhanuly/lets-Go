a1564cf053de8c218ed3e8af8b38be9362aded09
anurzhanuly <isaxved@gmail.com>
2021-09-26 17:39:39 +0600

    adds db insertion operation to create handler

--- cmd/web/handlers.go
+++ cmd/web/handlers.go
@@ -49,14 +49,21 @@ func (app *application) showSnippet(w http.ResponseWriter, r *http.Request) {
 func (app *application) createSnippet(w http.ResponseWriter, r *http.Request) {
 	if r.Method != http.MethodPost {
 		w.Header().Set("Allow", http.MethodPost)
-
 		app.clientError(w, http.StatusMethodNotAllowed)
 
 		return
 	}
 
-	_, err := w.Write([]byte("createSnippet"))
+	title := "O snail"
+	content := "O snail\nClimb Mount Fuji,\nBut slowly, slowly!\n\n– Kobayashi Issa"
+	expires := "7"
+
+	id, err := app.snippets.Insert(title, content, expires)
 	if err != nil {
-		app.errorLog.Println(err.Error())
+		app.serverError(w, err)
+
+		return
 	}
+
+	http.Redirect(w, r, fmt.Sprintf("/snippet?id=%d", id), http.StatusSeeOther)
 }
