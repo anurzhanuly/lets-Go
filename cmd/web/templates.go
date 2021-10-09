74cbb22a5913c666622edcdc28b56ecb81723c36
anurzhanuly <isaxved@gmail.com>
2021-10-09 15:54:37 +0600

    adding custom functions to template rendering

--- cmd/web/templates.go
+++ cmd/web/templates.go
@@ -4,12 +4,24 @@ import (
 	"anurzhanuly/snippetbox/pkg/models"
 	"html/template"
 	"path/filepath"
+	"time"
 )
 
 type templateData struct {
 	CurrentYear int
 	Snippet     *models.Snippet
 	Snippets    []*models.Snippet
+}
+
+func humanDate(t time.Time) string {
+	return t.Format("02 Jan 2006 at 15:04")
+}
+
+// Initialize a template.FuncMap object and store it in a global variable. This is
+// essentially a string-keyed map which acts as a lookup between the names of our
+// custom template functions and the functions themselves.
+var functions = template.FuncMap{
+	"humanDate": humanDate,
 }
 
 func newTemplateCache(dir string) (map[string]*template.Template, error) {
@@ -24,7 +36,7 @@ func newTemplateCache(dir string) (map[string]*template.Template, error) {
 
 		name := filepath.Base(page)
 
-		ts, err := template.ParseFiles(page)
+		ts, err := template.New(name).Funcs(functions).ParseFiles(page)
 		if err != nil {
 			return nil, err
 		}
